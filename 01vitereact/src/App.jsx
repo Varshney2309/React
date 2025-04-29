a6234496a9e3916dbe38b6744e188933e257c6ae
Varshney2309 <harshitvarshney3002@gmail.com>
2025-04-29 19:00:41 +0530

    basics

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,12 @@
+import Chai from "./chai"
+
+function App() {
+  
+
+  return (
+    <Chai />
+    // <h1>chai aur react with vite | Harshit Varshney</h1>
+  )
+}
+
+export default App
---
+++ 01vitereact/src/chai.jsx
@@ -0,0 +1,12 @@
+
+
+function Chai(){
+    return (
+        <h2>varshney learning react</h2>
+        // <h3>Chai aur code</h3>
+    )
+}
+
+export default Chai
+
+// always create function name with first letter uppercase
